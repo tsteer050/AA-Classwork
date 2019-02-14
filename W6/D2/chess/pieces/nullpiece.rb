c56ba335d47b6a9160cd29698c6d3b98aa7d17c2
tsteer <trevor.piano@gmail.com>
2019-02-13 19:13:02 -0800

    Added null piece class

---
+++ W6/D2/chess/pieces/nullpiece.rb
@@ -0,0 +1,20 @@
+require 'singleton'
+require_relative 'piece'
+
+class NullPiece < Piece
+
+    include Singleton
+
+    def initialize
+
+    end
+
+    def symbol
+        return "_"
+    end 
+
+    def moves
+        []
+    end
+
+end
