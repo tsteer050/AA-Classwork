8dcb99117bb5af1cfb06b274b07fe475cdc5aff3
tsteer <trevor.piano@gmail.com>
2019-02-19 14:03:08 -0800

    Completed p04

--- W7/D2/hash map lru cache/lib/p04_linked_list.rb
+++ W7/D2/hash map lru cache/lib/p04_linked_list.rb
@@ -1,3 +1,6 @@
+require 'byebug'
+
+
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -20,7 +23,13 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,34 +38,64 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail && @tail.prev == @head
   end
 
   def get(key)
+    self.each { |node| return node.val if node.key == key }
   end
 
   def include?(key)
+    self.each { |node| return true if node.key == key }
+    false
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    unless include?(key)
+      prev_node = last
+      prev_node.next = node
+      node.prev = prev_node
+      node.next = @tail
+      @tail.prev = node
+    end
   end
 
   def update(key, val)
+    self.each { |node| node.val = val if node.key == key }
   end
 
   def remove(key)
+    self.each do |node|
+      if node.key == key 
+        prev_node = node.prev 
+        next_node = node.next 
+        prev_node.next = next_node 
+        next_node.prev = prev_node
+      end
+    end
   end
 
-  def each
+  def each(&prc)
+    current_node = @head.next
+    until current_node == @tail
+      prc.call(current_node)
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
+
+
