9fb5604ee35a8c0a74c96389e2cbcde8c39838f4
tsteer <trevor.piano@gmail.com>
2019-02-19 16:31:34 -0800

    Some implementation of p06

--- W7/D2/hash map lru cache/lib/p06_lru_cache.rb
+++ W7/D2/hash map lru cache/lib/p06_lru_cache.rb
@@ -2,7 +2,7 @@ require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
 
 class LRUCache
-  def initialize(max, prc)
+  def initialize(max, prc=Proc.new { |x| x ** 2 })
     @map = HashMap.new
     @store = LinkedList.new
     @max = max
@@ -13,7 +13,18 @@ class LRUCache
     @map.count
   end
 
+  [key, node]
+
   def get(key)
+    if @map.include?(key)
+      node = @map[key]
+      update_node!(node)
+    else
+            #add to map and cache
+      
+
+      
+    end
   end
 
   def to_s
@@ -23,13 +34,44 @@ class LRUCache
   private
 
   def calc!(key)
-    # suggested helper method; insert an (un-cached) key
+    @prc.call(key)
   end
 
   def update_node!(node)
-    # suggested helper method; move a node to the end of the list
+    @store.remove(node.key)
+    @store.append(node.key, node.val)
   end
 
   def eject!
   end
 end
+
+# # Instructions:
+# # Let's say we're building an LRU cache that's going to cache the values
+#  of the perfect squares. So our LRU cache will store a @prc, which in this 
+#  case will be . If we don't have the value of any 
+#  number's square, we'll use this Proc to actually compute it. But we 
+#  don't want to compute it for the same number twice, so after I compute 
+#  anything, I'll store it in my LRU cache. But if my LRU Cache gets an
+#   input that doesn't exist in the cache, it'll compute it using the Proc.
+# # Now build your LRU cache. Every time you add a new key-value pair to
+#  your cache, you'll take two steps.
+# # First, look to see if the key points to any node in your hash map. 
+# If it does, that means that the item exists in your cache. Before you 
+# return the value from the node though, move the node to the very end 
+# of the list (since it's now the most recently used item).
+# # Say the key isn't in your hash map. That means it's not in your 
+# cache, so you need to compute its value and then add it. That has two parts.
+# # First, you call the proc using your key as input; the output will 
+# be your key's value. Append that key-value pair to the linked list 
+# (since, again, it's now the most recently used item). Then, add the 
+# key to your hash, along with a pointer to the new node. Finally, you 
+# have to check if the cache has exceeded its max size. If so, call 
+# theeject! function, which should delete the least recently used item 
+# so your LRU cache is back to max size.
+# # Hint: to delete that item, you have to delete the first item in your 
+# linked list, and delete that key from your hash. Implemented correctly, 
+# these should both happen in O(1) time.
+# # And you did it! Congratulations! Celebrate by getting a code review!
+
+
