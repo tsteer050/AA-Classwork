68190f6316adde0e69786e09289749a0aca8da1b
tsteer <trevor.piano@gmail.com>
2019-02-28 13:58:57 -0800

    Completed 02 searchable

--- W8/D4/skeleton/lib/02_searchable.rb
+++ W8/D4/skeleton/lib/02_searchable.rb
@@ -1,12 +1,25 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
+require 'byebug'
 
 module Searchable
   def where(params)
-    # ...
+    # debugger
+    where_string = params.keys.map { |param| "#{param} = ?"}.join(" AND ")
+    data = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_string}
+    SQL
+    self.parse_all(data)
   end
 end
 
-class SQLObject
-  # Mixin Searchable here...
+class SQLObject 
+  extend Searchable
+  
 end
+
